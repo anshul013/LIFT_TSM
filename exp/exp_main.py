98deae1980cffc3ebdbd200863db379bb88a8a7a
anshul013 <ultimatenanshul@gmail.com>
2025-07-10 17:25:08 +0200

    Added prefetch files and modified basic and main exp files

--- exp/exp_main.py
+++ exp/exp_main.py
@@ -9,6 +9,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 from torch import optim
+import importlib
 
 import os
 import time
@@ -22,32 +23,50 @@ warnings.filterwarnings('ignore')
 class Exp_Main(Exp_Basic):
     def __init__(self, args):
         super(Exp_Main, self).__init__(args)
-
-    def _build_model(self):
-        model_dict = {
-            'Autoformer': Autoformer,
-            'Transformer': Transformer,
-            'Informer': Informer,
-            'DLinear': DLinear,
-            'NLinear': NLinear,
-            'Linear': Linear,
-            'TSMixer' : TSMixer,
-            'GatedLTSF' : GatedLTSF,
-            'PatchTSMixer' : PatchTSMixer,
-            'PatchConv' : PatchConv,
-            'TSMixerChannelAttention' : TSMixerChannelAttention,
-            'GatedTSMixer': GatedTSMixer,
-            'AttentionMixer': AttentionMixer,
-            'SciNet':      SCINet,
-            'ViTPatchConv': ViTPatchConv,
-            'LIFTTSMixer': LIFTTSMixer
-        }
-
         self.non_transformer_model_list = {'DLinear', 'NLinear', 'Linear', 'TSMixer', 'GatedLTSF', 'PatchTSMixer', 'PatchConv', 'TSMixerChannelAttention', 'GatedTSMixer', 'AttentionMixer', 'SciNet', 'ViTPatchConv', 'LIFTTSMixer'}
-        model = model_dict[self.args.model].Model(self.args).float()
+
+    def _build_model(self, model=None, framework_class=None):
+        if model is None:
+            if self.args.model.endswith('_Ensemble'):
+                model = importlib.import_module(f'models.{self.args.model[:-len("_Ensemble")]}').Model_Ensemble(
+                    self.args).float()
+            else:
+                model = importlib.import_module(f'models.{self.args.model}').Model(self.args).float()
+
+        if hasattr(self.args, 'load_path'):
+            if not self.args.freeze:
+                self.model_optim = self._select_optimizer(
+                    model=model.to(self.device))
+            print('Load checkpoints from', self.args.load_path)
+            model = self.load_checkpoint(self.args.load_path, model)
+            if self.model_optim is not None:
+                print('Learning rate of model_optim is', self.model_optim.param_groups[0]['lr'])
+            if self.args.freeze:
+                model.requires_grad_(False)
+
+        if framework_class is not None:
+            model_params = sum([param.nelement() for param in model.parameters()])
+            model = framework_class(model, self.args)
+            new_model_params = sum([param.nelement() for param in model.parameters()])
+            trainable_params = sum([param.nelement() if param.requires_grad else 0 for param in model.parameters()])
+            print(f'Number of Params: {model_params} -> {new_model_params} (+{new_model_params - model_params})')
+            print(f'Trainable Params: {trainable_params}', '({:.1f}%)'.format(trainable_params / model_params * 100))
+            self.model_params = model_params
+            if self.model_optim is not None:
+                param_set = set()
+                for group in self.model_optim.param_groups:
+                    param_set.update(set(group['params']))
+                new_params = list(filter(lambda p: p not in param_set and p.requires_grad, model.parameters()))
+                if len(new_params) > 0:
+                    self.model_optim.add_param_group({'params': new_params})
 
         if self.args.use_multi_gpu and self.args.use_gpu:
             model = nn.DataParallel(model, device_ids=self.args.device_ids)
+        elif hasattr(self.args, 'local_rank') and self.args.local_rank != -1:
+            model = model.to(self.device)
+            model = nn.parallel.DistributedDataParallel(model, device_ids=[self.args.local_rank],
+                                                      output_device=self.args.local_rank,
+                                                      find_unused_parameters=True)
         return model
 
     def _get_data(self, flag):
@@ -79,7 +98,7 @@ class Exp_Main(Exp_Basic):
                 # encoder - decoder
                 if self.args.use_amp:
                     with torch.cuda.amp.autocast():
-                        if self.args.model in self.non_transformer_model_list :
+                        if self.args.model in self.non_transformer_model_list:
                             outputs = self.model(batch_x)
                         else:
                             if self.args.output_attention:
@@ -88,9 +107,9 @@ class Exp_Main(Exp_Basic):
                                 outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
                 else:
                     if self.args.model in self.non_transformer_model_list:
-                        if self.args.model == "TSMixerChannelAttention" : 
+                        if self.args.model == "TSMixerChannelAttention":
                             outputs = self.model(batch_x)[0]
-                        else :
+                        else:
                             outputs = self.model(batch_x)
                     else:
                         if self.args.output_attention:
@@ -119,7 +138,6 @@ class Exp_Main(Exp_Basic):
 
         path = os.path.join(self.args.checkpoints, setting)
         if not os.path.exists(path):
-            print(path)
             os.makedirs(path)
 
         time_now = time.time()
@@ -265,14 +283,13 @@ class Exp_Main(Exp_Basic):
                                 outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
                 else:
                     if self.args.model in self.non_transformer_model_list:
-                        if self.args.model == "TSMixerChannelAttention" : 
+                        if self.args.model == "TSMixerChannelAttention": 
                             outputs, attention_weight = self.model(batch_x)
-                        else :
+                        else:
                             outputs = self.model(batch_x)
                     else:
                         if self.args.output_attention:
                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-
                         else:
                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
 
@@ -285,7 +302,7 @@ class Exp_Main(Exp_Basic):
                 pred = outputs  # outputs.detach().cpu().numpy()  # .squeeze()
                 true = batch_y  # batch_y.detach().cpu().numpy()  # .squeeze()
 
-                if self.args.model == "TSMixerChannelAttention" : 
+                if self.args.model == "TSMixerChannelAttention":
                     attention_weight = attention_weight.detach().cpu().numpy()
                     attention_weights.append(attention_weight)
                 preds.append(pred)
@@ -312,7 +329,7 @@ class Exp_Main(Exp_Basic):
 
         np.save(folder_path + 'pred.npy', preds)
             
-        if self.args.model == "TSMixerChannelAttention" :
+        if self.args.model == "TSMixerChannelAttention":
             attention_weights = np.concatenate(attention_weights, axis=0)
             attention_weights = np.array(attention_weights)
             attention_weights = np.average(attention_weights, axis=0)
@@ -332,6 +349,7 @@ class Exp_Main(Exp_Basic):
             self.model.load_state_dict(torch.load(best_model_path))
 
         preds = []
+
         self.model.eval()
         with torch.no_grad():
             for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
@@ -347,9 +365,9 @@ class Exp_Main(Exp_Basic):
                 if self.args.use_amp:
                     with torch.cuda.amp.autocast():
                         if self.args.model in self.non_transformer_model_list:
-                            if self.args.model == "TSMixerChannelAttention" :
+                            if self.args.model == "TSMixerChannelAttention":
                                 outputs, attention_weight = self.model(batch_x)
-                            else :
+                            else:
                                 outputs = self.model(batch_x)
                         else:
                             if self.args.output_attention:
@@ -366,7 +384,7 @@ class Exp_Main(Exp_Basic):
                             outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
                 pred = outputs.detach().cpu().numpy()  # .squeeze()
                 preds.append(pred)
-        
+
         preds = np.array(preds)
         preds = np.concatenate(preds, axis=0)
         if (pred_data.scale):
@@ -381,7 +399,7 @@ class Exp_Main(Exp_Basic):
         pd.DataFrame(np.append(np.transpose([pred_data.future_dates]), preds[0], axis=1), columns=pred_data.cols).to_csv(folder_path + 'real_prediction.csv', index=False)
 
         #If the model is attention-based -> save the attention weights for further analysis
-        if self.args.model == "TSMixerChannelAttention" :
+        if self.args.model == "TSMixerChannelAttention":
             np.save(folder_path + 'attention_weights.npy', self.model.attention_weights)
             plt.imsave(folder_path + 'attention_weights_heat_map.png', 
                        self.model.attention_weights, cmap='hot')
